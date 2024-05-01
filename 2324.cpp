55fe22a8afdb976a23384afed61bee9dc0bdfc8c
Utkarsh Mishra <121924685+mutkarsh06@users.noreply.github.com>
2024-05-01 07:32:53 +0530

    Add files via upload

---
+++ 2324.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /*Loop in C++:
+    1.while loop
+    2.For Loop  in C++
+    3.do while loop*/
+    for(int i=1;i<=10;i++){
+   cout<<i<<endl;
+    }
+    return 0;
+}
